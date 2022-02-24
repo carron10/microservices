a1f2505700c014ec0e6af28a767ae661a5a21687
carron10 <carronmuleya10@gmail.com>
2022-02-24 21:58:05 +0200

    First Commit

---
+++ src/main/java/zw/co/chatsfly/Chatsfly_service_1/ChatsflyServiceOneApplication.java
@@ -0,0 +1,26 @@
+package zw.co.chatsfly.Chatsfly_service_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class ChatsflyServiceOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatsflyServiceOneApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    String test() {
+        return "Client 1";
+    }
+    @RequestMapping("/hello")
+    String test1() {
+        return "Hello World";
+    }
+}
---
+++ src/test/java/zw/co/chatsfly/Chatsfly_service_1/ChatsflyService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package zw.co.chatsfly.Chatsfly_service_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatsflyService1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
