1ff26aa056b0b93087556a241d46e098d7f9504d
carron10 <carronmuleya10@gmail.com>
2022-03-23 21:11:30 +0200

    First commit

---
+++ src/main/java/zw/co/chatsfly/Eureka_server_learn/EurekaServerLearnApplication.java
@@ -0,0 +1,17 @@
+package zw.co.chatsfly.Eureka_server_learn;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableAdminServer
+@EnableEurekaServer
+public class EurekaServerLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerLearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zw/co/chatsfly/Eureka_server_learn/EurekaServerLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package zw.co.chatsfly.Eureka_server_learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
