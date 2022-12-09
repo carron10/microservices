2e24dde5c5bb4c8a5850b2f77cbd298edd2c13bc
carron10 <77936477+carron10@users.noreply.github.com>
2022-12-09 13:58:55 +0200

    jjh

--- src/main/java/zw/co/chatsfly/Eureka_server_learn/EurekaServerLearnApplication.java
+++ src/main/java/zw/co/chatsfly/Eureka_server_learn/EurekaServerLearnApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@Configuration
 @EnableAutoConfiguration
 @EnableDiscoveryClient
 @EnableAdminServer
