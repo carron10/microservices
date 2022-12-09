4b40372560e4f63c6a02c6608d1789675715c4f2
carron10 <77936477+carron10@users.noreply.github.com>
2022-12-09 14:14:15 +0200

    bcvfxcv

--- src/main/java/zw/co/chatsfly/Eureka_server_learn/EurekaServerLearnApplication.java
+++ src/main/java/zw/co/chatsfly/Eureka_server_learn/EurekaServerLearnApplication.java
@@ -4,10 +4,13 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@SpringBootApplication
 @EnableAutoConfiguration
 @EnableDiscoveryClient
 @EnableAdminServer
+@EnableEurekaServer
 public class EurekaServerLearnApplication {
 
 	public static void main(String[] args) {
